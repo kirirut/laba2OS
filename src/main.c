e121c709147ea013dff39673740ae38a2b6199b2
kirirut <rutkkir@gmail.com>
2025-02-26 14:21:49 +0300

    structure

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <locale.h>
+int main(int argc,char *argv[],char *envp[])
+{
+
+
+
+
+
+}
